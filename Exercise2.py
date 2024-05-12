0a8f8e05ca326770677336cb597c965bf7cc27ad
PhurpaDorji <02230072.cst@rub.edu.bt>
2024-05-12 11:42:03 -0600

    its me phurpa

---
+++ Exercise2.py
@@ -0,0 +1,25 @@
+student_list= []
+student_dict= {}
+
+sthdent_name= input("Enter your name").capitalize
+student_age= input("Enter your age")
+student_grade= input("What is your standard or your grade")
+print("Student information  has been added successfully")
+
+dict_item([('student_name', {"age", student_age and "grade", student_grade})])
+
+name=input("Enter the name of the student that you are looking for:\n").capitalize
+if name == student_name:
+    print(dict_item)
+
+else:
+    print("Not been found")
+
+a=input("Enter the name u want to remove").capitalize
+if remove_name in student_list:
+    student_list.remove(remove_name)
+    del book_dict[remove_name]
+    print("Name removred successfully!")
+
+else:
+    print("Name not found")
